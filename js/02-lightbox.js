995fd458b62622cd7c5acd04b381a4a60fb33c43
Anton Kulakov <92113717+AntonK33@users.noreply.github.com>
2022-11-26 12:27:49 +0200

    Второе задание выполнено

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(({ preview, original, description }) => {
+    const link = document.createElement('a');
+    const imgItem = document.createElement('img');
+    imgItem.src = preview;
+    imgItem.alt = description;
+    imgItem.classList.add('gallery__image');
+    link.classList.add('gallery__item');
+    link.href = original;
+    link.append(imgItem);
+    gallery.append(link);
+});
+
+
+
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
