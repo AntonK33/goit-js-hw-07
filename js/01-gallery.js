b4e3db7b04b9ca3bbd0ebd96585c3158b05abb4e
Anton Kulakov <92113717+AntonK33@users.noreply.github.com>
2022-12-03 19:17:48 +0200

    правки от ментора

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,31 +9,46 @@ const imgModal = document.querySelector('.modal');
 const imgTag = document.querySelector('.imgTag');
 
 //const image =  document.createElement('img');
-galleryItems.forEach(({ preview, original, description }) => {
-    const link = document.createElement('a');
-    const imgItem = document.createElement('img');
-    imgItem.src = preview;
-    imgItem.dataSource = original; 
-    imgItem.alt = description;
-    imgItem.classList.add('gallery__image');
-    link.classList.add('gallery__link');
-    link.href = original;
-    link.append(imgItem);
-    gallery.append(link);
-});
+//galleryItems.forEach(({ preview, original, description }) => {
+//    const link = document.createElement('a');
+ //   const imgItem = document.createElement('img');
+ //   imgItem.src = preview;
+ //   imgItem.dataSource = original; 
+  //  imgItem.alt = description;
+ //   imgItem.classList.add('gallery__image');
+ //   link.classList.add('gallery__link');
+//    link.href = original;
+ //   link.append(imgItem);
+//    gallery.append(link);
+//});
+ 
+const gList = galleryItems.map(({ preview, original, description }) => `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`).join("");
+gallery.insertAdjacentHTML("beforeend",gList);
 
 gallery.addEventListener('click', openModal);
+
   function  openModal (e)  {
     e.preventDefault();
      if (!e.target.classList.contains("gallery__image")) {
     return;
   }
 
-//const urlOriginal = e.target.dataset.source;
+const urlOriginal = e.target.dataset.source;
  const instance = basicLightbox.create(
     `
     <div class="modal">
-       <img src="${e.target.dataSource}" alt="${e.target.alt}" width="800" height="600">
+       <img src="${urlOriginal}" alt="${e.target.alt}" width="800" height="600">
        <a>Close</a>
     </div>
 `,
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,20 +4,24 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
 
-galleryItems.forEach(({ preview, original, description }) => {
-    const link = document.createElement('a');
-    const imgItem = document.createElement('img');
-    imgItem.src = preview;
-    imgItem.alt = description;
-    imgItem.classList.add('gallery__image');
-    link.classList.add('gallery__item');
-    link.href = original;
-    link.append(imgItem);
-    gallery.append(link);
-});
+//galleryItems.forEach(({ preview, original, description }) => {
+//    const link = document.createElement('a');
+//    const imgItem = document.createElement('img');
+ //   imgItem.src = preview;
+//    imgItem.alt = description;
+ //   imgItem.classList.add('gallery__image');
+//    link.classList.add('gallery__item');
+//    link.href = original;
+//    link.append(imgItem);
+//    gallery.append(link);
+//});
 
-
-
+const galList = galleryItems.map(({ preview, original, description }) =>
+  `  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+).join("");
+gallery.insertAdjacentHTML('beforeend', galList);
 
 const lightbox = new SimpleLightbox(".gallery__item", {
   captionsData: "alt",
